78fb054739bf920a599aa8eb740f42c39133fc35
Abdulrahman Shahzad <AbdulrahmanShahzadlife@gmail.com>
2022-09-24 23:52:46 -0400

    Activity 4, part 2 completed

--- Activity4.py
+++ Activity4.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email
 from flask_bootstrap import Bootstrap
 
 from flask import Flask, render_template, session, redirect, url_for, flash
@@ -11,16 +11,30 @@ bootstrap = Bootstrap(app)
 
 class NameForm(FlaskForm):
  name = StringField('What is your name?', validators=[DataRequired()])
+ email = StringField('What is your UoFT Email address?', validators=[DataRequired(), Email()])
+
  submit = SubmitField('Submit')
 
+
 @app.route('/', methods=['GET', 'POST'])
+
 def index():
     form = NameForm()
     if form.validate_on_submit():
         old_name = session.get('name')
+        old_email = session.get('email')
+       
+        
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
+
+        if old_email is not None and old_email != form.email.data:
+
+            flash('Looks like you have changed your Email!')
+
         session['name'] = form.name.data
+        session['email'] = form.email.data
+
         return redirect(url_for('index'))
-    return render_template('index2.html',
-        form = form, name = session.get('name'))
+
+    return render_template('index2.html', form = form, name = session.get('name'), email = session.get('email') )   
